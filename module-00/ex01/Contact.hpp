46f36839d513beec59180cccdda18d7133c8cf2e
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-06-27 10:57:11 -0300

    feat: Contact represents a phone book contact

--- module-00/ex01/Contact.hpp
+++ module-00/ex01/Contact.hpp
@@ -6,18 +6,25 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 10:09:16 by mdias-ma          #+#    #+#             */
-/*   Updated: 2023/06/27 10:49:54 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2023/06/27 10:56:22 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
 
+# include <string>
+
 class Contact
 {
 public:
 
 private:
+	std::string	firstName;
+	std::string	lastName;
+	std::string	nickName;
+	std::string	phoneNumber;
+	std::string	darkestSecret;
 };
 
 #endif // !CONTACT_HPP
