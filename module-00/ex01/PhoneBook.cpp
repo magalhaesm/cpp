cc9c1183c5ccc8b1a36b417ef736dda6608575c3
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-06-28 17:37:19 -0300

    refact: display messages through the user interface

--- module-00/ex01/PhoneBook.cpp
+++ module-00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 20:34:06 by mdias-ma          #+#    #+#             */
-/*   Updated: 2023/06/28 14:58:22 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2023/06/28 17:34:41 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,13 @@ void	PhoneBook::addContact()
 		validationContact.setNickName(ui.getUserInput("Nickname: "));
 		validationContact.setPhoneNumber(ui.getUserInput("Phone number: "));
 		validationContact.setDarkestSecret(ui.getUserInput("Darkest secret: "));
-	} catch (const std::runtime_error& e) {
-		std::cout << e.what() << std::endl;
+	} catch (const std::runtime_error& err) {
+		ui.showMessage(err.what());
 		return ;
 	}
 	contacts[contactIndex] = validationContact;
 	contactIndex++;
+	ui.showMessage("Contact added!");
 }
 
 void	PhoneBook::searchContact()
