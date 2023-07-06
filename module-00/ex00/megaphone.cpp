71299f0453b3f53a20357335330ebbb325221eeb
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-07-05 22:06:56 -0300

    style: format megaphone

--- module-00/ex00/megaphone.cpp
+++ module-00/ex00/megaphone.cpp
@@ -6,39 +6,39 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 11:01:40 by mdias-ma          #+#    #+#             */
-/*   Updated: 2023/06/26 20:22:41 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2023/07/05 14:41:36 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
 #include <iostream>
 
-std::string	uppercase(std::string);
+std::string uppercase(std::string);
 
-int	main(int argc, const char **argv)
+int main(int argc, const char** argv)
 {
-	std::string str;
+    std::string str;
 
-	if (argc == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (EXIT_SUCCESS);
-	}
-	for (int i = 1; i < argc; i++)
-	{
-		str = argv[i];
-		std::cout << uppercase(str);
-	}
-	std::cout << std::endl;
-	return (EXIT_SUCCESS);
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (EXIT_SUCCESS);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        str = argv[i];
+        std::cout << uppercase(str);
+    }
+    std::cout << std::endl;
+    return (EXIT_SUCCESS);
 }
 
-std::string	uppercase(std::string str)
+std::string uppercase(std::string str)
 {
-	size_t	length = str.length();
+    size_t length = str.length();
 
-	for (size_t j = 0; j < length; j++)
-		str[j] = std::toupper(str[j]);
+    for (size_t j = 0; j < length; j++)
+        str[j] = std::toupper(str[j]);
 
-	return (str);
+    return (str);
 }
