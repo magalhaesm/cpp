4753c32b6c105c1d5e6f1744ee3ce7ed357f494d
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-07-20 15:41:32 -0300

    feat: add ex02

---
+++ module-02/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/18 15:54:30 by mdias-ma          #+#    #+#             */
+/*   Updated: 2023/07/20 11:48:33 by mdias-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ios>
+#include <iostream>
+
+#include "Fixed.hpp"
+
+int main(void)
+{
+    Fixed a;
+    Fixed const b(Fixed(5.05f) * Fixed(2));
+
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max(a, b) << std::endl;
+}
