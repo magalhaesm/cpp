9659e24721e6aeefac3703af20dae89ccd00b51c
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-08-03 10:59:14 -0300

    refactor: remove unneeded function

--- module-06/ex00/ScalarConverter.cpp
+++ module-06/ex00/ScalarConverter.cpp
@@ -6,19 +6,17 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 13:22:11 by mdias-ma          #+#    #+#             */
-/*   Updated: 2023/08/03 09:31:14 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2023/08/03 10:56:19 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <climits>
-#include <cstdlib>
 #include <limits>
+#include <cstdlib>
 #include <iostream>
 
 #include "Parser.hpp"
 #include "ScalarConverter.hpp"
 
-static bool isInRange(long, long, long);
 static double extractPseudoLiteral(const std::string&);
 
 const ScalarConverter::ConversionEntry ScalarConverter::conversionTable[functions] = {
@@ -52,13 +50,14 @@ void ScalarConverter::charConversion(const std::string& literal)
 void ScalarConverter::intConversion(const std::string& literal)
 {
     ConversionPack pack = newPack();
-    long value = std::atol(literal.c_str());
+    int value = std::atoi(literal.c_str());
+    long longValue = std::atol(literal.c_str());
 
     pack.char_value = static_cast<char>(value);
-    pack.is_char_valid = isInRange(value, CHAR_MIN, CHAR_MAX);
+    pack.is_char_valid = (pack.char_value == longValue);
 
-    pack.int_value = static_cast<int>(value);
-    pack.is_int_valid = (pack.int_value == value);
+    pack.int_value = value;
+    pack.is_int_valid = (pack.int_value == longValue);
 
     pack.float_value = static_cast<float>(value);
     pack.double_value = static_cast<double>(value);
@@ -70,14 +69,15 @@ void ScalarConverter::floatConversion(const std::string& literal)
 {
     ConversionPack pack = newPack();
     float value = std::atof(literal.c_str());
+    long longValue = std::atol(literal.c_str());
 
     pack.char_value = static_cast<char>(value);
-    pack.is_char_valid = isInRange(value, CHAR_MIN, CHAR_MAX);
+    pack.is_char_valid = (pack.char_value == longValue);
 
     pack.int_value = static_cast<int>(value);
-    pack.is_int_valid = isInRange(static_cast<long>(value), INT_MIN, INT_MAX);
+    pack.is_int_valid = (pack.int_value == longValue);
 
-    pack.float_value = static_cast<float>(value);
+    pack.float_value = value;
     pack.double_value = static_cast<double>(value);
 
     std::cout << pack << std::endl;
@@ -87,15 +87,16 @@ void ScalarConverter::doubleConversion(const std::string& literal)
 {
     ConversionPack pack = newPack();
     double value = std::strtod(literal.c_str(), 0);
+    long longValue = static_cast<long>(value);
 
     pack.char_value = static_cast<char>(value);
-    pack.is_char_valid = isInRange(value, CHAR_MIN, CHAR_MAX);
+    pack.is_char_valid = (pack.char_value == longValue);
 
     pack.int_value = static_cast<int>(value);
-    pack.is_int_valid = isInRange(static_cast<long>(value), INT_MIN, INT_MAX);
+    pack.is_int_valid = (pack.int_value == longValue);
 
     pack.float_value = static_cast<float>(value);
-    pack.double_value = static_cast<double>(value);
+    pack.double_value = value;
 
     std::cout << pack << std::endl;
 }
@@ -104,15 +105,16 @@ void ScalarConverter::pseudoLiteralConversion(const std::string& literal)
 {
     ConversionPack pack = newPack();
     double value = extractPseudoLiteral(literal);
+    long longValue = static_cast<long>(value);
 
     pack.char_value = static_cast<char>(value);
-    pack.is_char_valid = isInRange(value, CHAR_MIN, CHAR_MAX);
+    pack.is_char_valid = (pack.char_value == longValue);
 
     pack.int_value = static_cast<int>(value);
-    pack.is_int_valid = isInRange(static_cast<long>(value), INT_MIN, INT_MAX);
+    pack.is_int_valid = (pack.int_value == longValue);
 
     pack.float_value = static_cast<float>(value);
-    pack.double_value = static_cast<double>(value);
+    pack.double_value = value;
 
     std::cout << pack << std::endl;
 }
@@ -127,11 +129,6 @@ void ScalarConverter::invalidConversion(const std::string&)
     pack.is_double_valid = false;
 
     std::cout << pack << std::endl;
-}
-
-static bool isInRange(long value, long min, long max)
-{
-    return (value >= min && value <= max);
 }
 
 static double extractPseudoLiteral(const std::string& literal)
