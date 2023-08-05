59c7324047f20cd1fb0b67891e5773f3dfd15ebe
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-08-05 17:42:50 -0300

    feat(module-07): add ex00

---
+++ module-07/ex00/whatever.hpp
@@ -0,0 +1,26 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void swap(T& x, T& y)
+{
+    T temp;
+
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+template <typename T>
+const T& min(const T& x, const T& y)
+{
+    return (x < y) ? x : y;
+}
+
+template <typename T>
+const T& max(const T& x, const T& y)
+{
+    return (x > y) ? x : y;
+}
+
+#endif // !WHATEVER_HPP
