a2cf6eefea9b5cd0a35bb11dca7ca9900462af7d
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-08-13 17:47:02 -0300

    fix(module-01): use zombie functions

--- module-01/ex00/main.cpp
+++ module-01/ex00/main.cpp
@@ -6,17 +6,23 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 22:10:18 by mdias-ma          #+#    #+#             */
-/*   Updated: 2023/07/05 14:33:46 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2023/08/12 15:57:13 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+void randomChump(std::string name);
+Zombie* newZombie(std::string name);
+
 int main(void)
 {
     Zombie murphy("Murphy");
 
-    Zombie* teddy = new Zombie("Teddy");
+    Zombie* teddy = newZombie("Teddy");
+
+    randomChump("Foo");
+
     delete teddy;
 
     return 0;
