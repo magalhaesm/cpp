dbefa919bfbd0e3f7fb7a3e43a56c3c323053d2f
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-08-30 18:42:13 -0300

    tests(module-09): test iteration

--- module-09/ex02/tests.cpp
+++ module-09/ex02/tests.cpp
@@ -1,11 +1,14 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-#include <deque>
+#include <list>
 #include <vector>
+#include <cmath>
 #include <numeric>
 
 #include "PmergeMe.hpp"
+#include "Slice.hpp"
+#include "utils.hpp"
 
 TEST_CASE("isSorted function tests")
 {
@@ -15,7 +18,7 @@ TEST_CASE("isSorted function tests")
         std::iota(vec.begin(), vec.end(), 0);
         CHECK(PmergeMe::isSorted(vec.begin(), vec.end()));
 
-        std::deque<int> lst(10);
+        std::list<int> lst(10);
         std::iota(lst.begin(), lst.end(), 0);
         CHECK(PmergeMe::isSorted(lst.begin(), lst.end()));
     }
@@ -27,9 +30,47 @@ TEST_CASE("isSorted function tests")
         vec[5] = 100;
         CHECK_FALSE(PmergeMe::isSorted(vec.begin(), vec.end()));
 
-        std::deque<int> lst(10);
+        std::list<int> lst(10);
         std::iota(lst.begin(), lst.end(), 0);
-        lst[5] = 100;
+        lst.insert(lst.begin(), 100);
         CHECK_FALSE(PmergeMe::isSorted(lst.begin(), lst.end()));
+    }
+}
+
+TEST_CASE("Slice tests")
+{
+    std::vector<int> xs = {
+
+        8, 7, 100, 27, 46, 11, 53, 2, 13, 0, 15, 5, 17, 18, 14, 28, 20, 19, 21
+    };
+
+    SUBCASE("Iteration")
+    {
+        size_t iterations = std::log2(xs.size());
+
+        size_t size = 1;
+        while (iterations--)
+        {
+            Slice<std::vector<int>::iterator> slice(xs.begin(), size);
+
+            size_t groups = size * 2;
+            size_t length = xs.size() - (xs.size() % groups);
+
+            for (size_t it = 0; it < length; it += groups, slice += 2)
+            {
+                CHECK(xs[it + size - 1] == slice[0]);
+            }
+            size *= 2;
+        }
+    }
+
+    SUBCASE("Using + operator")
+    {
+        Slice<std::vector<int>::iterator> slice(xs.begin(), 1);
+
+        for (size_t idx = 0; idx < xs.size(); ++idx)
+        {
+            CHECK(xs[idx] == *(slice + idx));
+        }
     }
 }
