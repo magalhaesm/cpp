d7ed8f6f4adeadb75272d8b6d454c55a58a12687
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-09-04 15:51:22 -0300

    refactor: split the algorithm into functions

--- module-09/ex02/PmergeMe.hpp
+++ module-09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 12:41:01 by mdias-ma          #+#    #+#             */
-/*   Updated: 2023/09/02 16:56:20 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2023/09/04 15:41:10 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <vector>
 #include <algorithm>
 
+#include "Slice.hpp"
 #include "utils.hpp"
 
 namespace PmergeMe
@@ -24,41 +25,66 @@ namespace PmergeMe
     template <typename Iterator>
     void sort(Iterator first, Iterator last);
 
-    template <typename Slice>
-    void sort_impl(Slice first, Slice last);
+    template <typename Slice, typename Compare>
+    void sort_impl(Slice first, Slice last, Compare cmp);
 
-    template <typename ForwardIterator>
-    bool isSorted(ForwardIterator first, ForwardIterator last);
+    template <typename Container, typename Function>
+    void each_insertion_for(const Container& pend, Function inserter);
+
+    template <typename Slice>
+    void collect_sorted(Slice& first, std::list<Slice>& chain);
+
+    namespace impl
+    {
+        template <typename T, typename Compare>
+        struct Inserter;
+    }
 };
 
 template <typename Iterator>
 void PmergeMe::sort(Iterator first, Iterator last)
 {
-    sort_impl(make_slice(first, 1), make_slice(last, 1));
+    typedef Slice<Iterator> Slice;
+    sort_impl(Slice(first), Slice(last), std::less<Slice>());
 }
 
-template <typename T>
-struct Inserter
+template <typename Slice, typename Compare>
+void PmergeMe::sort_impl(Slice first, Slice last, Compare cmp)
 {
-    std::vector<T>& m_chain;
-
-    Inserter(std::vector<T>& chain)
-        : m_chain(chain)
+    size_t size = std::distance(first, last);
+    if (size < 2)
     {
+        return;
     }
 
-    void operator()(const T& element, size_t max_insertion_idx)
-    {
-        typedef typename std::vector<T>::iterator Iterator;
+    bool odd = (size % 2 != 0);
+    Slice end = odd ? utils::prev(last) : last;
 
-        Iterator end = m_chain.begin() + std::min(max_insertion_idx, m_chain.size());
-        Iterator idx = std::upper_bound(m_chain.begin(), end, element);
-        m_chain.insert(idx, element);
+    swap_adjacent(first, end, cmp);
+    sort_impl(Slice(first, 2), Slice(end, 2), cmp);
+
+    std::list<Slice> chain;
+    std::vector<Slice> pend;
+
+    for (Slice it = first; it != end; it += 2)
+    {
+        pend.push_back(it);
+        chain.push_back(it + 1);
     }
-};
+
+    if (odd)
+    {
+        pend.push_back(utils::prev(last));
+    }
+
+    impl::Inserter<Slice, Compare> inserter(chain, cmp);
+    each_insertion_for(pend, inserter);
+
+    collect_sorted(first, chain);
+}
 
 template <typename Container, typename Function>
-void each_insertion_for(const Container& pend, Function inserter)
+void PmergeMe::each_insertion_for(const Container& pend, Function inserter)
 {
     inserter(pend[0], 0);
 
@@ -82,75 +108,46 @@ void each_insertion_for(const Container& pend, Function inserter)
 }
 
 template <typename Slice>
-void PmergeMe::sort_impl(Slice first, Slice last)
+void PmergeMe::collect_sorted(Slice& first, std::list<Slice>& chain)
 {
-    size_t size = std::distance(first, last);
-    if (size < 2)
-    {
-        return;
-    }
-
-    bool odd = (size % 2 != 0);
-    Slice end = odd ? utils::prev(last) : last;
-
-    for (Slice it = first; it != end; it += 2)
-    {
-        if (*(it + 1) < *it)
-        {
-            iter_swap(it + 1, it);
-        }
-    }
-
-    sort_impl(make_slice(first, 2), make_slice(end, 2));
-
-    std::vector<Slice> chain;
-    std::vector<Slice> pend;
-
-    for (Slice it = first; it != end; it += 2)
-    {
-        pend.push_back(it);
-        chain.push_back(it + 1);
-    }
-
-    if (odd)
-    {
-        pend.push_back(utils::prev(last));
-    }
-
-    Inserter<Slice> inserter(chain);
-    each_insertion_for(pend, inserter);
+    typedef typename std::list<Slice>::iterator Iterator;
 
     std::vector<typename Slice::value_type> sorted;
 
-    for (size_t i = 0; i < chain.size(); ++i)
+    for (Iterator it = chain.begin(); it != chain.end(); ++it)
     {
-        Slice slice = chain[i];
-        for (size_t j = 0; j < slice.size(); ++j)
+        Slice slice = *it;
+        for (size_t idx = 0; idx < slice.size(); ++idx)
         {
-            sorted.push_back(*(slice.base() + j));
+            sorted.push_back(*(utils::next(slice.base(), idx)));
         }
     }
     std::copy(sorted.begin(), sorted.end(), first.base());
 }
 
-template <typename ForwardIterator>
-bool PmergeMe::isSorted(ForwardIterator first, ForwardIterator last)
+template <typename T, typename Compare>
+struct PmergeMe::impl::Inserter
 {
-    if (first == last)
+    std::list<T>& m_chain;
+    Compare m_cmp;
+
+    Inserter(std::list<T>& chain, Compare cmp)
+        : m_chain(chain)
+        , m_cmp(cmp)
     {
-        return true;
     }
 
-    ForwardIterator next = first;
-    while (++next != last)
+    void operator()(const T& element, size_t maxInsertionIdx)
     {
-        if (*next < *first)
-        {
-            return false;
-        }
-        ++first;
+        typedef typename std::list<T>::iterator Iterator;
+
+        size_t maxIdx = std::min(maxInsertionIdx, m_chain.size());
+        Iterator end = utils::next(m_chain.begin(), maxIdx);
+
+        Iterator idx = std::upper_bound(m_chain.begin(), end, element, m_cmp);
+
+        m_chain.insert(idx, element);
     }
-    return true;
-}
+};
 
 #endif // !PMERGEME_HPP
