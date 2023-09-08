d9a8e25af7019866154c8ef91765b7923987489d
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-09-08 20:47:50 -0300

    feat(module-08): add more tests to ex00

--- module-08/ex00/main.cpp
+++ module-08/ex00/main.cpp
@@ -2,29 +2,50 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include <list>
 
 #include "easyfind.hpp"
 
 int main(void)
 {
-    std::vector<int> vec;
-
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(5);
-    vec.push_back(3);
-    vec.push_back(4);
-
-    std::sort(vec.begin(), vec.end());
-    std::cout << std::distance(vec.begin(), vec.end()) << std::endl;
-    std::vector<int>::iterator foundIter = easyfind(vec, 10);
-
-    if (foundIter != vec.end())
     {
-        std::cout << "Found: " << *foundIter << std::endl;
+        std::vector<int> xs;
+
+        xs.push_back(1);
+        xs.push_back(2);
+        xs.push_back(5);
+        xs.push_back(3);
+        xs.push_back(4);
+
+        std::vector<int>::iterator foundIter = easyfind(xs, 10);
+
+        if (foundIter != xs.end())
+        {
+            std::cout << "Found: " << *foundIter << std::endl;
+        }
+        else
+        {
+            std::cout << "Not found" << std::endl;
+        }
     }
-    else
     {
-        std::cout << "Not found" << std::endl;
+        std::list<int> xs;
+
+        xs.push_back(1);
+        xs.push_back(2);
+        xs.push_back(5);
+        xs.push_back(3);
+        xs.push_back(4);
+
+        std::list<int>::iterator foundIter = easyfind(xs, 10);
+
+        if (foundIter != xs.end())
+        {
+            std::cout << "Found: " << *foundIter << std::endl;
+        }
+        else
+        {
+            std::cout << "Not found" << std::endl;
+        }
     }
 }
